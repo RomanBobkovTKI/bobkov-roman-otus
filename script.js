46d424f5fe74fd7eddb6ee84d01f3d46099f9cc9
RomanBobkovTKI <bob0504@mail.ru>
2023-06-26 22:05:55 +0300

    Переделал обработку под стримы, кажется...

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 const stream = require('stream');
 const fs = require('fs')
-const readline = require('readline');
+const { pipeline, Transform } = require('stream');
+const { promisify } = require('util');
 
+const pipelineAsync = promisify(pipeline);
 
 async function read(filePath) {
     
     //Создание стримов
     const readableStream = fs.createReadStream(filePath, 'utf-8');
     const writableStream = fs.createWriteStream('output_file.txt', 'utf-8')
-    //Создание нужных переменных
-    const buffers = []
+    const myTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            //Создание нужных переменных
+            const buffers = []
+            buffers.push(chunk.toString())
 
-    //Обработчик ошибок
-    readableStream.on('error', function (error) {
-        console.log(`error: ${error.message}`);
-    })
+            //Разделяем на строки
+            let array = buffers[buffers.length-1].split('\r\n')
 
-    //Чанки складываем в строку
-    for await (const chunk of readableStream) {
-        buffers.push(chunk.toString())
-    }
+            for (let i = 0; i < array.length; i++ ){
+                //Разделяем на отдельные слова
+                array[i] = array[i].split(/\s+/);
+                //Сортируем по алфавиту
+                array[i] = array[i].sort((a, b) => a.localeCompare(b))
+                
+                //Обрабатываем спец символвы
+                for(let j = 0; j < array[i].length; j++){
+                    array[i][j] = array[i][j].replace(/,\s*/, '')
+                }
 
-    //Разделяем на строки
-    let array = buffers[buffers.length-1].split('\r\n')
+                //Подсчитаем количество элементов
+                array[i] = array[i].reduce((acc, i) => {
+                    if (acc.hasOwnProperty(i)) {
+                    acc[i] += 1;
+                    } else {
+                    acc[i] = 1;
+                    }
+                    return acc;
+                },{})
+            
+                //Возвращаем массив значений
+                array[i] = Object.values(array[i])
 
-    for (let i = 0; i < array.length; i++ ){
-        //Разделяем на отдельные слова
-        array[i] = array[i].split(/\s+/);
-        //Сортируем по алфавиту
-        array[i] = array[i].sort((a, b) => a.localeCompare(b))
-        
-        //Обрабатываем спец символвы
-        for(let j = 0; j < array[i].length; j++){
-            array[i][j] = array[i][j].replace(/,\s*/, '')
-        }
-
-        //Подсчитаем количество элементов
-        array[i] = array[i].reduce((acc, i) => {
-            if (acc.hasOwnProperty(i)) {
-              acc[i] += 1;
-            } else {
-              acc[i] = 1;
             }
-            return acc;
-        },{})
-    
-        //Возвращаем массив значений
-        array[i] = Object.values(array[i])
 
-    }
-
-    //Записываем результат в файл
-    for (let i = 0; i < array.length; i++ ) {
-        writableStream.write(array[i] + '\n')
-    }
+            for (let i = 0; i < array.length; i++ ) {
+                array[i] = (array[i] + '\n')
+            }
+            callback(null, array.join('').toString())
+        }
+    })
     
+
+    try {
+        await pipelineAsync(
+            readableStream,
+            myTransform,
+            writableStream
+        )
+        console.log('Обработка завершена.')
+    }
+    catch (err) {
+        console.error('Ошибка:', err);
+    }
     
 }
 
