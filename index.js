d158f63b2092fd94ed8021ce04cf2640ebbbcd3d
RomanBobkovTKI <bob0504@mail.ru>
2023-07-18 12:16:49 +0300

    Подключил скрипт app.js в footer

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
     partialsDir: [
-        path.join(__dirname, 'html/layouts/partials')
+        path.join(__dirname, 'views/layouts/partials')
     ]
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
-app.set('views', 'html')
+app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
